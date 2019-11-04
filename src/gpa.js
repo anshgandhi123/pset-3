c79fb1fa8f69b60af9990bc361f5863a1da30bb4
Ansh Gandhi <agandhi543@gmail.com>
2019-11-04 13:17:46 -0500

    commit

---
+++ src/gpa.js
@@ -0,0 +1,45 @@
+const readlineSync = require("readline-sync");
+letterA = 4;
+letterB = 3;
+letterC = 2;
+letterD = 1;
+letterF = 0;
+
+let grade = (readlineSync.question("\nEnter a letter grade: "));
+
+if (grade == "A+") {
+  console.log("\nYour GPA is 4.00.");
+}
+else if (grade == "A") {
+  console.log("\nYour GPA is 4.00.");
+}
+else if (grade == "A-") {
+  console.log("\nYour GPA is 3.67.");
+}
+else if (grade == "B+") {
+  console.log("\nYour GPA is 3.33.");
+}
+else if (grade == "B") {
+  console.log("\nYour GPA is 3.00.");
+}
+else if (grade == "B-") {
+  console.log("\nYour GPA is 2.67.");
+}
+else if (grade == "C+") {
+  console.log("\nYour GPA is 2.33.");
+}
+else if (grade == "C") {
+  console.log("\nYour GPA is 2.00.");
+}
+else if (grade == "C-") {
+  console.log("\nYour GPA is 1.67.");
+}
+else if (grade == "D+") {
+  console.log("\nYour GPA is 1.33.");
+}
+else if (grade == "D") {
+  console.log("\nYour GPA is 1.00");
+}
+else if (grade == "F") {
+  console.log("\nYour GPA is 0.00.");
+}
--- src/sequence.js
+++ src/sequence.js
@@ -1,26 +1,41 @@
 const readlineSync = require("readline-sync");
-let amount = parseFloat(readlineSync.question("\nEnter three numbers: \n\n"));
-let secondAmount = parseFloat(readlineSync.question(""));
-let thirdAmount = parseFloat(readlineSync.question(""));
+let amount = (readlineSync.question("\nEnter three numbers: \n\n"));
+let secondAmount = (readlineSync.question(""));
+let thirdAmount = (readlineSync.question(""));
 
-if (amount < Number.MIN_SAFE_INTEGER) {
- console.log("\nInvalid.");
-} else if (amount > Number.MAX_SAFE_INTEGER) {
+if (amount < Number.MIN_SAFE_INTEGER || amount > Number.MAX_SAFE_INTEGER) {
   console.log("\nInvalid.");
-} else if (thirdAmount>secondAmount>amount) {
+}else if (secondAmount < Number.MIN_SAFE_INTEGER || secondAmount > Number.MAX_SAFE_INTEGER) {
+  console.log("\nInvalid.");
+}else if (thirdAmount < Number.MIN_SAFE_INTEGER || thirdAmount > Number.MAX_SAFE_INTEGER) {
+   console.log("\nInvalid.");
+}
+ else if ((thirdAmount>secondAmount) && (secondAmount>amount)) {
   console.log("\nStrictly increasing.");
-} else if (thirdAmount>amount>secondAmount){
+} else if ((thirdAmount>secondAmount) && (secondAmount==amount)){
   console.log("\nIncreasing");
 }
-  else if (thirdAmount=amount=secondAmount) {
+else if ((secondAmount>amount) && (thirdAmount==secondAmount)){
+  console.log("\nIncreasing");
+ }
+  else if ((thirdAmount==secondAmount) && (amount==secondAmount)) {
     console.log("\nEqual.");
   }
-  else if (thirdAmount<amount) {
-    console.log("\nDecreasing.");
+  else if ((secondAmount<amount) && (thirdAmount==secondAmount)){
+    console.log("\nDecreasing");
   }
-  else if (thirdAmount<secondAmount<amount) {
+  else if ((secondAmount==amount) && (secondAmount>thirdAmount)) {
+    console.log("\nDecreasing");
+  }
+  else if ((thirdAmount<secondAmount) && (secondAmount<amount)) {
     console.log("\nStrictly descreasing.");
   }
-  else if (thirdAmount<amount<secondAmount  || amount<thirdamount<secondamount) {
+  else if ((secondAmount<amount) && (secondAmount<thirdAmount)) {
+    console.log("\nUnordered.");
+  }
+  else if ((thirdAmount<secondAmount) && (secondAmount>amount)){
+    console.log("\nUnordered.");
+  }
+  else if ((thirdAmount>secondAmount) && (amount>secondAmount)) {
     console.log("\nUnordered.");
   }
